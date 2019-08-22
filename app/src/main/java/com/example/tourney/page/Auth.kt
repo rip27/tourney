376571db8b083fae064dfce2640fb87d286ecef1
Pepe <rizal27pangestu@gmail.com>
2019-08-22 15:40:31 +0700

    .

--- app/src/main/java/com/example/tourney/page/Auth.kt
+++ app/src/main/java/com/example/tourney/page/Auth.kt
@@ -3,8 +3,12 @@ package com.example.tourney.page
 import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
+import android.text.method.HideReturnsTransformationMethod
+import android.text.method.PasswordTransformationMethod
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
+import android.widget.CheckBox
 import android.widget.EditText
 import android.widget.Spinner
 import android.widget.Toast
@@ -12,28 +16,42 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.tourney.R
 import com.example.tourney.data.Pref
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.*
 import kotlinx.android.synthetic.main.login.*
 
 class Auth : AppCompatActivity() {
 
     lateinit var pref: Pref
-    private lateinit var fAuth: FirebaseAuth
+    lateinit var fAuth: FirebaseAuth
     lateinit var dbRef: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
-        buttonLogin.setOnClickListener {
+        pref = Pref(this)
+        fAuth = FirebaseAuth.getInstance()
+        hideShowPass.setOnCheckedChangeListener { compoundButton, b ->
+            if (compoundButton.isChecked) {
+                editTextPasswordLogin.transformationMethod = HideReturnsTransformationMethod.getInstance()
+            } else {
+                editTextPasswordLogin.transformationMethod = PasswordTransformationMethod.getInstance()
+            }
+        }
 
+        buttonLogin.setOnClickListener {
+            buttonLogin.visibility = View.GONE
+            progressLogin.visibility = View.VISIBLE
             val email = editTextEmailLogin.text.toString()
             val password = editTextPasswordLogin.text.toString()
             if (email.isEmpty() || password.isEmpty()) {
                 Toast.makeText(this, "Fill All Data", Toast.LENGTH_SHORT).show()
+                buttonLogin.visibility = View.VISIBLE
+                progressLogin.visibility = View.GONE
             } else {
-                pref.setStatus(true)
                 fAuth.signInWithEmailAndPassword(email, password)
                     .addOnSuccessListener {
+                        pref.setStatus(true)
                         FirebaseDatabase.getInstance().getReference("user")
                             .addListenerForSingleValueEvent(object : ValueEventListener {
                                 override fun onCancelled(p0: DatabaseError) {
@@ -42,12 +60,7 @@ class Auth : AppCompatActivity() {
 
                                 override fun onDataChange(p0: DataSnapshot) {
                                     val user = fAuth.currentUser
-                                    if (user != null) {
-                                        pref.saveUID(user.uid)
-                                        startActivity(Intent(this@Auth, Dashboard::class.java))
-                                    } else {
-                                        Log.e("TAG_ERROR", "USER NOT FOUND")
-                                    }
+                                    updateUI(user)
                                 }
 
                             })
@@ -58,16 +71,28 @@ class Auth : AppCompatActivity() {
                             "Username atau Password salah!",
                             Toast.LENGTH_SHORT
                         ).show()
+                        buttonLogin.visibility = View.VISIBLE
+                        progressLogin.visibility = View.GONE
                     }
             }
         }
         daftarAkun.setOnClickListener {
+
             var dialog: AlertDialog
             val alertDialog = AlertDialog.Builder(this)
             val view = LayoutInflater.from(this).inflate(R.layout.daftar, null)
             alertDialog.setView(view)
             alertDialog.setTitle("DAFTAR")
-            alertDialog.setPositiveButton("YES") { dialog, i ->
+            alertDialog.setPositiveButton("DAFTAR") { dialog, i ->
+                val hideShowPassRegister = view.findViewById<CheckBox>(R.id.hideShowPassRegister)
+                val etpassreg = view.findViewById<EditText>(R.id.editTextPasswordRegister)
+                hideShowPassRegister.setOnCheckedChangeListener { compoundButton, b ->
+                    if (compoundButton.isChecked) {
+                        etpassreg.transformationMethod = HideReturnsTransformationMethod.getInstance()
+                    } else {
+                        etpassreg.transformationMethod = PasswordTransformationMethod.getInstance()
+                    }
+                }
                 val name = view.findViewById<EditText>(R.id.editTextNameRegister).text.toString()
                 val phone = view.findViewById<EditText>(R.id.editTextPhoneRegister).text.toString()
                 val gender = view.findViewById<Spinner>(R.id.sp_gender).selectedItem.toString()
@@ -76,7 +101,15 @@ class Auth : AppCompatActivity() {
                 if (name.isEmpty() || phone.isEmpty() || gender.isEmpty() || email.isEmpty() || password.isEmpty()) {
                     Toast.makeText(this, "Fill All Data", Toast.LENGTH_SHORT).show()
                 } else {
-                    addUserToFirebase(name, phone, gender, email, password)
+                    fAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener {
+                            if (it.isSuccessful) {
+                                addUserToFirebase(name, phone, gender, email, password)
+                                Toast.makeText(this, "Register Berhasil!", Toast.LENGTH_SHORT).show()
+                            } else {
+                                Toast.makeText(this, "GAGAL", Toast.LENGTH_SHORT).show()
+                            }
+                        }
                 }
             }
             alertDialog.setNegativeButton("NO") { dialog, i ->
@@ -85,8 +118,6 @@ class Auth : AppCompatActivity() {
             dialog = alertDialog.create()
             dialog.show()
         }
-        pref = Pref(this)
-        fAuth = FirebaseAuth.getInstance()
         if (!pref.cekStatus()!!) {
 
         } else {
@@ -102,24 +133,31 @@ class Auth : AppCompatActivity() {
 
     private fun addUserToFirebase(name: String, phone: String, gender: String, email: String, password: String) {
         val uid = fAuth.currentUser?.uid
-        fAuth.createUserWithEmailAndPassword(email, password)
-            .addOnSuccessListener {
-                dbRef = FirebaseDatabase.getInstance().getReference("user/$uid")
-                dbRef.child("/id").setValue(uid)
-                dbRef.child("/name").setValue(name)
-                dbRef.child("/phone").setValue(phone)
-                dbRef.child("/gender").setValue(gender)
-                dbRef.child("/email").setValue(email)
-                dbRef.child("/password").setValue(password)
-                    .addOnSuccessListener {
-                        Toast.makeText(this, "Sukses Daftar", Toast.LENGTH_SHORT).show()
-                    }
-                    .addOnFailureListener {
-                        Toast.makeText(this, "Gagal Daftar", Toast.LENGTH_SHORT).show()
-                    }
-            }
-            .addOnFailureListener {
-                Toast.makeText(this, "Gagal Daftar", Toast.LENGTH_SHORT).show()
-            }
+        dbRef = FirebaseDatabase.getInstance().getReference("user/$uid")
+        dbRef.child("/id").setValue(uid)
+        dbRef.child("/name").setValue(name)
+        dbRef.child("/phone").setValue(phone)
+        dbRef.child("/gender").setValue(gender)
+        dbRef.child("/email").setValue(email)
+        dbRef.child("/password").setValue(password)
+    }
+
+    //
+//    override fun onStart() {
+//        super.onStart()
+//        val user = FirebaseAuth.getInstance().currentUser
+//        if (user != null) {
+//            updateUI(user)
+//            finish()
+//        }
+//    }
+//
+    fun updateUI(user: FirebaseUser?) {
+        if (user != null) {
+            pref.saveUID(user.uid)
+            startActivity(Intent(this, Dashboard::class.java))
+        } else {
+            Log.e("TAG_ERROR", "user tidak ada")
+        }
     }
 }
--- app/src/main/java/com/example/tourney/page/Dashboard.kt
+++ app/src/main/java/com/example/tourney/page/Dashboard.kt
@@ -2,6 +2,7 @@ package com.example.tourney.page
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
@@ -9,17 +10,29 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.tourney.R
+import com.example.tourney.adapter.TournamentAdapter
 import com.example.tourney.data.Pref
+import com.example.tourney.model.TournamentModel
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.navigation.NavigationView
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
+import kotlinx.android.synthetic.main.nav_header_dashboard.*
+import java.util.*
 
 class Dashboard : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
+    lateinit var dbRef: DatabaseReference
     lateinit var fAuth: FirebaseAuth
     lateinit var pref: Pref
+    private var tournamentAdapter: TournamentAdapter? = null
+    private var recyclerView: RecyclerView? = null
+    private var list: MutableList<TournamentModel> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,9 +43,9 @@ class Dashboard : AppCompatActivity(), NavigationView.OnNavigationItemSelectedLi
         fAuth = FirebaseAuth.getInstance()
         pref = Pref(this)
 
-        val fab: FloatingActionButton = findViewById(R.id.fab)
+        val fab: FloatingActionButton = findViewById(R.id.addTournament)
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+            Snackbar.make(view, "${fAuth.uid}", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
@@ -45,7 +58,76 @@ class Dashboard : AppCompatActivity(), NavigationView.OnNavigationItemSelectedLi
         drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
 
+        FirebaseDatabase.getInstance().getReference("user/${fAuth.uid}")
+            .child("profile").addListenerForSingleValueEvent(object : ValueEventListener {
+
+                override fun onDataChange(p0: DataSnapshot) {
+                    Glide.with(this@Dashboard).load(p0.value.toString())
+                        .centerCrop()
+                        .error(R.mipmap.ic_launcher)
+                        .into(profileOnDashboard)
+                }
+
+                override fun onCancelled(p0: DatabaseError) {
+
+                }
+            })
+
+
+        FirebaseDatabase.getInstance().getReference("user/${pref.getUID()}")
+            .child("name").addListenerForSingleValueEvent(object : ValueEventListener {
+
+                override fun onDataChange(p0: DataSnapshot) {
+                    nameOnDashboard.text = p0.value.toString()
+                }
+
+                override fun onCancelled(p0: DatabaseError) {
+
+                }
+            })
+        FirebaseDatabase.getInstance().getReference("user/${fAuth.uid}")
+            .child("email").addListenerForSingleValueEvent(object : ValueEventListener {
+
+                override fun onDataChange(p0: DataSnapshot) {
+                    emailOnDashboard.text = p0.value.toString()
+                }
+
+                override fun onCancelled(p0: DatabaseError) {
+
+                }
+            })
+
         navView.setNavigationItemSelectedListener(this)
+
+        var linearLayoutManager = LinearLayoutManager(this)
+        recyclerView = findViewById(R.id.recyclerViewTournament)
+        recyclerView!!.layoutManager = linearLayoutManager
+        recyclerView!!.setHasFixedSize(true)
+
+        fAuth = FirebaseAuth.getInstance()
+
+        dbRef = FirebaseDatabase.getInstance()
+            .reference.child("tournament")
+        dbRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(p0: DataSnapshot) {
+                list = ArrayList()
+                for (dataSnapshot in p0.children) {
+                    val addDataAll = dataSnapshot.getValue(
+                        TournamentModel::class.java
+                    )
+                    addDataAll!!.key = dataSnapshot.key
+                    list.add(addDataAll)
+                    tournamentAdapter = TournamentAdapter(this@Dashboard, list)
+                    recyclerView!!.adapter = tournamentAdapter
+                }
+            }
+
+            override fun onCancelled(p0: DatabaseError) {
+                Log.e(
+                    "TAG_ERROR", p0.message
+                )
+            }
+        })
     }
 
     override fun onBackPressed() {
